ce0fa3ba9bd9f78ced03114310ffa9393d27fd75
Jackson Passos <jacksonguitar33@gmail.com>
2021-06-08 00:42:21 -0300

    :art: [perf] componentizando
    components summary and dashboard

---
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -0,0 +1,15 @@
+import logoImg from '../../assets/logo.svg'
+import { Summary } from '../Summary';
+
+import { Container } from "./styles";
+
+export function Dashboard() {
+    return(
+        <>
+        <Container>
+            <Summary />
+        </Container>
+
+        </>
+    )
+}
---
+++ dtmoney/src/components/Summary/index.tsx
@@ -0,0 +1,9 @@
+import logoImg from '../../assets/logo.svg'
+
+import { Container } from "./styles";
+
+export function Summary() {
+    return(
+        <h1>Summary</h1>
+    )
+}
